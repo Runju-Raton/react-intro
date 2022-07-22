2dac7bcd555c5eb2f6908eb271cc5907614cd78e
Runju-Raton <runju.raton@gmail.com>
2022-07-22 12:07:46 +0600

    create first react component

--- react.js
+++ react.js
@@ -1,7 +1,20 @@
-const element = (
-    <div>
-        <h1>Learn React, Love React.</h1>
-    </div>
-);
+const Increment = ()=>{
+    return (
+        <div>
+            <h1>Learn React, Love React.</h1>
+            <h2>Count: 0</h2>
+            <button>Click</button>
+            <br></br><br></br><br></br>
+        </div>
+    )
+}
+   
 const container=document.querySelector('#root');
-ReactDOM.render(element,container);
+ReactDOM.render(
+    <div>
+        <Increment></Increment>
+        <Increment></Increment>
+        <Increment></Increment>
+    </div>,
+container);
+
